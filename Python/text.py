cfa6f35b040e214b85341ae2042c6414d0074b43
Giancarlo <giancarlobenavides@hotmail.com>
2020-10-29 13:32:56 -0500

    add jupyter

---
+++ Python/text.py
@@ -0,0 +1,18 @@
+import math as m
+from math import pi
+import numpy as np
+import pylab as pl
+
+x1=(1+m.sqrt(5))/2
+x2=m.e
+
+y1=round(x3,3)
+y2=m.floor(x3)
+y3=m.ceil(x3)
+
+print('Número áureo: ', x1)
+print('Número euler: ', x2)
+print('Número pi: ', x3)
+print('redondear: ', y1)
+print('redondear: ', y2)
+print('redondear: ', y3)

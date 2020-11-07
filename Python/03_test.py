cc4ee6796b57d16ef8977037181b78e81baca18d
Giancarlo <giancarlobenavides@hotmail.com>
2020-11-07 01:23:32 -0500

    Update 03_test.py

--- Python/03_test.py
+++ Python/03_test.py
@@ -1,7 +1,16 @@
 # test_01.py 
 ''' para el curso de métodos numéricos
 por Ing. Giancarlo Ortiz '''
-# Importando módulos
+# Importando módulos biblioteca estandar
 import math
 from math import pi
-import numpy as np
+
+# Entrada estandar
+radio = input("Elige el radio del circulo [cm]:    ")
+radio = int(radio)
+area=pi*pow(radio, 2)
+
+# Salida estándar
+print("-----------------------------------------")
+print(f"El area del circulo: {area:7.2f} cm")
+print("-----------------------------------------")

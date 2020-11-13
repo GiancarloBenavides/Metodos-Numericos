8c7b8011e10bae7d080e925c64cd6aabd402c43e
Giancarlo <giancarlobenavides@hotmail.com>
2020-11-13 10:27:14 -0500

    graficas

--- Python/09_test.py
+++ Python/09_test.py
@@ -3,16 +3,14 @@
 Ejemplo para el curso de métodos numéricos
 por Ing. Giancarlo Ortiz '''
 # Instalar módulos
-''' Las funciones en Python se definen con la palabra clave def, seguida del nombre de la función.
-Sus parámetros se escriben entre parentesis y pueden incluir valores por defecto.
-Otra forma de escribir funciones, aunque menos utilizada, es con la palabra clave lambda.
-El valor devuelto en las funciones con def será el dado con la instrucción return. '''
+''' La generación de gráficos En Python requiere módulos adicionales.
+Los vectores se pueden generar a partir de datos contenidos en listas o arrays.
+la operación numérica es posible  con NumPy y la generación de estos gráficos con Matplotlib. '''
 # Importar módulos instalados
 import numpy as np
 from numpy import exp, pi
 from math import sqrt
 import matplotlib.pyplot as plt
-
 # Periodo
 amplitud_max = 10
 coeficiente_de_amortiguamiento = 0.07
@@ -21,25 +19,31 @@ longitud = 10
 gravedad = 9.8
 periodo = 2 * pi * sqrt(longitud/gravedad)
 # frecuencia
-frecuencia = 2 * pi / periodo
+frecuencia = 1 / periodo
+angular = 2 * pi * frecuencia
 # Vector de tiempo
 tiempo = np.linspace(0, 60, 256, endpoint=True)
 # Vectores de posición
 K = amplitud_max * exp(-coeficiente_de_amortiguamiento * tiempo)
-position = K * np.cos(frecuencia * tiempo)
-velocidad = K * np.sin(frecuencia * tiempo)
+position = K * np.cos(angular * tiempo)
+velocidad = K * np.sin(angular * tiempo)
 # Gráficas
 plt.plot(tiempo, position, label="Posición(\u03B8)")
-plt.plot(tiempo, velocidad, label="Velocidad(clsgit)")
-# Propiedades de las gráficas
+plt.plot(tiempo, velocidad, label="Velocidad(\u03B8)")
+plt.legend()
+# Propiedades de las gráficas - Texto
 plt.title("Posición y Velocidad del Péndulo Simple")
 plt.xlabel("tiempo [sg]")
 plt.ylabel("amplitud [grados]")
 plt.xlim(0, 60)
+# Lineas de división
 plt.xticks([0, 10, 20, 30, 40, 50, 60])
 plt.yticks([-1.5 * amplitud_max, 0, 1.5 * amplitud_max])
-
-plt.legend()
+# Expresiones matemáticas con látex
+plt.text(35, -8, r'$T=2\pi\sqrt{\frac{l}{g}}=$' + f"{periodo:2.2f} sg", fontsize=9)
+plt.text(35, -10, r'$f=\displaystyle\frac{l}{T}=$' + f"{frecuencia:2.2f} hz", fontsize=9)
+plt.text(35, -12, r'$\omega=2\pi f=$' + f"{angular:2.2f} radianes/sg", fontsize=9)
+# Salida en pantalla via Qt
 plt.show()
-# Salida estándar
+# Salida estándar por consola
 print("----------------------------------------------------")
